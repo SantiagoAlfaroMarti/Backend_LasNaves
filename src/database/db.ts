396aec296f2fbc445ec574058c0fbe0b4eb820ae
unknown <santiagoalfamar@gmail.com>
2024-10-01 11:56:28 +0200

    feat: create db.ts

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,23 +1,26 @@
-import { DataSource } from 'typeorm';
-import { Person } from '../entities/person/person.entity';
-import { Room } from '../entities/room/room.entity';
-import { Access } from '../entities/access/access.entity';
-import 'reflect-metadata';
-
-// Importa las demás entidades que necesites
+import "reflect-metadata";
+import 'dotenv/config';
+import { DataSource } from "typeorm";
+import { Person1726560167856 } from "./migrations/1726560167856-person";
+import { Room1726560205798 } from "./migrations/1726560205798-room";
+import { Access1726560218248 } from "./migrations/1726560218248-access";
+import { AccessHistory1726563877728 } from "./migrations/1726563877728-accessHistory";
+import { Administration1726564013782 } from "./migrations/1726564013782-administration";
+import { person } from "../entities/person/person";
+import { room } from "../entities/room/room";
+import { access } from "../entities/access/access";
+import { accessHistory } from "../entities/accessHistory/accessHistory";
+import { administration } from "../entities/administration/administration";
 
 export const AppDataSource = new DataSource({
-  type: 'mysql', // Cambia a 'mysql'
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3306, // Cambia a 3306 si usas MySQL por defecto
-  username: process.env.DB_USERNAME || 'root', // Asegúrate de que esto sea correcto
-  password: process.env.DB_PASSWORD || '123456789', // Asegúrate de que esto sea correcto
-  database: process.env.DB_DATABASE || 'LasNavesCoworking',
-  synchronize: false, // En producción, usa migraciones en lugar de synchronize.
-  logging: false,
-  entities: [Person, Room, Access], // Asegúrate de que están todas tus entidades
-  migrations: ['src/database/migrations/*.ts'],
-  subscribers: [],
-});
-
-
+    type: "mysql",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: [person, room, access, accessHistory, administration],
+    migrations: [Person1726560167856, Room1726560205798, Access1726560218248, AccessHistory1726563877728, Administration1726564013782],
+    synchronize: false,
+    logging: false,
+})
