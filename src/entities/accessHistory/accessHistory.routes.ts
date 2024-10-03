7545cf135ecb602803e4128d4d01b05934d7c591
unknown <santiagoalfamar@gmail.com>
2024-10-03 12:26:27 +0200

    update: accessHistory.routes.ts

--- src/entities/accessHistory/accessHistory.routes.ts
+++ src/entities/accessHistory/accessHistory.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { auth } from "../../middlewares/auth";
 import { getAccessHistories, getRoomAccessHistories } from "./accessHistory.controller";
+import { isAdmin } from "../../middlewares/isAdmin";
 
 const router = Router()
 
-router.get('/',auth, getAccessHistories)
-router.get('/room/:room_id',auth, getRoomAccessHistories)
+router.get('/',auth,isAdmin, getAccessHistories)
+router.get('/room/:room_id',auth,isAdmin, getRoomAccessHistories)
 
 export { router }
