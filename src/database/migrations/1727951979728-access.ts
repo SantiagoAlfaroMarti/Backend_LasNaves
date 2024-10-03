b432e3e81066d88072dbda4d13e7e62322f534ce
unknown <santiagoalfamar@gmail.com>
2024-10-03 12:52:53 +0200

    update: migrations access.ts

--- src/database/migrations/1727951979728-access.ts
+++ src/database/migrations/1727951979728-access.ts
@@ -1,11 +1,66 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class Access1727951979728 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "access",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "person_id",
+                        type: "int",
+                        isNullable: false,
+                    },
+                    {
+                        name: "room_id",
+                        type: "int",
+                        isNullable: false,
+                    },
+                    {
+                        name: "entry_datetime",
+                        type: "datetime",
+                        default: "CURRENT_TIMESTAMP", 
+                    },
+                    {
+                        name: "exit_datetime",
+                        type: "datetime",
+                        isNullable: true,
+                    },
+                    {
+                        name: "state",
+                        type: "enum",
+                        enum: ["active", "inactive", "cancelled"],
+                        default: "'active'",
+                    },
+                ],
+                foreignKeys: [
+                    {
+                        columnNames: ["person_id"],
+                        referencedTableName: "person",
+                        referencedColumnNames: ["id"],
+                        onDelete: "CASCADE"
+                    },
+                    {
+                        columnNames: ["room_id"],
+                        referencedTableName: "room",
+                        referencedColumnNames: ["id"],
+                        onDelete: "CASCADE"
+                    },
+                ],
+            }),
+            true 
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("access");
     }
-
 }
